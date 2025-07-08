a9027c9f6cca4461d55ff42a5f768c4eb9974e2d
jescarcon <jesuscardenas2000@gmail.com>
2025-07-08 16:57:54 +0200

    QuickFix

--- CoreApp/settings.py
+++ CoreApp/settings.py
@@ -110,8 +110,17 @@ DATABASES = {
     #     }
     # }
 }
+import os
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
+import pymysql
+pymysql.install_as_MySQLdb()
+
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
